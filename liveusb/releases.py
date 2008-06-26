294b3e06b835b7f1386d2fb830c454d314ad2ab9
Horst Tritremmel <peter_weber69@gmx.at>
2008-06-26 10:23:52 +0000

    * depends on python-dbus * iso links updated

--- liveusb/releases.py
+++ liveusb/releases.py
@@ -1,33 +1,54 @@
 releases = (
     {
-        'name': 'sidux-2008-02-erebos-pre1-kde-lite (i686)',
-        'url': 'http://debian.tu-bs.de/project/sidux/preview/sidux-2008-02-erebos-pre1-kde-lite-i386-200806032235.iso',
+        'name': 'sidux-2008-02-kde-lite-i386',
+        'url': 'http://debian.tu-bs.de/project/sidux/release/sidux-2008-02-erebos-kde-lite-i386-200806252044.iso',
     },
     {
-        'name': 'sidux-2008-02-erebos-pre1-kde-lite (amd64)',
-        'url': 'http://debian.tu-bs.de/project/sidux/preview/sidux-2008-02-erebos-pre1-kde-lite-amd64-200806032247.iso',
+        'name': 'sidux-2008-02-kde-lite-amd64',
+        'url': 'http://debian.tu-bs.de/project/sidux/release/sidux-2008-02-erebos-kde-lite-amd64-200806252050.iso',
     },
     {
-        'name': 'sidux-2008-01-nyx-kde-lite (i686)',
-        'url': 'http://debian.tu-bs.de/project/sidux/release/sidux-2008-01-nyx-kde-lite-i386-200804112335.iso',
+        'name': 'sidux-2008-02-kde-full-i386-amd64',
+        'url': 'http://debian.tu-bs.de/project/sidux/release/sidux-2008-02-erebos-kde-full-i386-amd64-200806251839.iso',
     },
     {
-        'name': 'sidux-2008-01-nyx-kde-lite (amd64)',
-        'url': 'http://debian.tu-bs.de/project/sidux/release/sidux-2008-01-nyx-kde-full-i386-amd64-200804120127.iso',
+        'name': 'sidux-2008-02-xfce-i386',
+        'url': 'http://debian.tu-bs.de/project/sidux/release/sidux-2008-02-erebos-xfce-i386-200806252100.iso',
     },
     {
-        'name': 'sidux-2008-01-nyx-kde-full (i686/amd64)',
-        'url': 'http://debian.tu-bs.de/project/sidux/release/sidux-2008-01-nyx-kde-full-i386-amd64-200804120127.iso',
+        'name': 'sidux-2008-02-xfce-amd64',
+        'url': 'http://debian.tu-bs.de/project/sidux/release/sidux-2008-02-erebos-xfce-amd64-200806252108.iso',
     },
 )
 
-
 '''
+
 from urlgrabber import *
+
 url = "http://debian.tu-bs.de/project/sidux/release"
-fo = urlopen(url)
-data = fo.read()
-for i in data.split('"'):
-    if i.endswith('iso'):
-        print i
+
+urlret  = "ok"
+release = ""
+try:
+    fo = urlopen(url)
+except:
+    print '%s not found!' % url
+    urlret = "no"
+    releases = (
+        {
+            'name': 'url not found)',
+            'url': 'http://debian.tu-bs.de/project/sidux/release',
+        }
+    )
+
+if urlret == 'ok':
+    data = fo.read()
+    for i in data.split('"'):
+        if i.endswith('iso'):
+            release = "%s{'url': '%s/%s', 'name': '%s'}, " % (release, url, i, i)
+
+
+releases = (release)
+
+print releases
 '''
